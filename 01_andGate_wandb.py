90987a22a539da3aed514a5c98a673dfc1c704cd
laz4rz <boronskimikolaj@gmail.com>
2023-04-05 01:09:25 +0200

    add random seed and surrogate gradient to wandb sweep

--- 01_andGate_wandb.py
+++ 01_andGate_wandb.py
@@ -37,6 +37,17 @@ def forward_pass(net, data, num_steps):
 
   return torch.stack(spk_rec), np.stack(mem_hist)
 
+def set_seed(seed: int = 42) -> None:
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    # When running on the CuDNN backend, two further options must be set
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    # Set a fixed value for the hash seed
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    print(f"Random seed set as {seed}")
 
 sweep_config = {
     'method': 'grid',
@@ -45,7 +56,7 @@ sweep_config = {
         'goal': 'minimize',
         'name': 'train loss'
     },
-    "description": "Lineaer(2, 8), Linear(8, 2), Leaky(spike_grad=None)",
+    "description": "Lineaer(2, 8), Linear(8, 2), Leaky()",
     'parameters': {
         'learning_rate': {
             'values': [0.0001, 0.001, 0.01, 0.1]
@@ -60,31 +71,37 @@ sweep_config = {
             'values': [0.1, 0.2, 0.5, 0.7, 0.9]
         },
         'timesteps': {
-            'values': [5, 10, 25, 50, 100]
+            'values': [5, 10, 25]
         },
         'epochs': {
-            'values': [10, 50, 100, 200, 300]
+            'values': [30]
         },
         'rates': {
             'values': [(0.9, 0.1), (0.8, 0.2), (0.7, 0.3), (0.6, 0.4)]
+        },
+        "surrogate": {
+            "values": [surrogate.fast_sigmoid(), surrogate.triangular(), surrogate.sigmoid(), surrogate.straight_through_estimator(), surrogate.spike_rate_escape(), surrogate.heaviside()]
+        },
+        'seed': {
+            'values': [1, 2137, 69]
         }
     }
 }
 sweep_id = wandb.sweep(sweep_config, project='and-gate-snn')
-np.random.seed(0)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
 def train():
   wandb.init()
   config = wandb.config
+  set_seed(config.seed)
   train_loader = DataLoader(and_generator(size=700), config.batch_size)
   test_loader = DataLoader(and_generator(size=300), config.batch_size)
 
   net = nn.Sequential(
       nn.Linear(2, 8),
       nn.Linear(8, 2),
-      snn.Leaky(beta=config.beta, threshold=config.threshold, init_hidden=True, output=True)
+      snn.Leaky(beta=config.beta, threshold=config.threshold, spike_grad=config.surrogate, init_hidden=True, output=True)
   )
   net = net.to(device)
 
@@ -92,22 +109,21 @@ def train():
   loss_fn = SF.mse_count_loss(correct_rate=config.rates[0], incorrect_rate=config.rates[1])
 
   for epoch in range(config.epochs):
-      for i, (data, targets) in enumerate(iter(train_loader)):
-          data = data.to(device)
-          targets = targets.squeeze().to(device)
+    for i, (data, targets) in enumerate(iter(train_loader)):
+        data = data.to(device)
+        targets = targets.squeeze().to(device)
 
-          net.train()
-          spk_rec, mem_hist = forward_pass(net, data, config.timesteps) # forward-pass
-          # return spk_rec, mem_hist
-          loss_val = loss_fn(spk_rec, targets) # loss calculation
-          optimizer.zero_grad() # null gradients
-          loss_val.backward() # calculate gradients
-          optimizer.step() # update weights
+        net.train()
+        spk_rec, mem_hist = forward_pass(net, data, config.timesteps) # forward-pass
+        loss_val = loss_fn(spk_rec, targets) # loss calculation
+        optimizer.zero_grad() # null gradients
+        loss_val.backward() # calculate gradients
+        optimizer.step() # update weights
 
-          wandb.log({
-            "epoch": epoch,
-            "train loss": loss_val.item(),
-            "accuracy": SF.accuracy_rate(spk_rec, targets)
-          })
+    wandb.log({
+    "epoch": epoch,
+    "train loss": loss_val.item(),
+    "accuracy": SF.accuracy_rate(spk_rec, targets)
+    })
 
 wandb.agent(sweep_id, train)
