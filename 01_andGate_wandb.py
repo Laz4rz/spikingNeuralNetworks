4bd45d08c4ae56d1c1956646ac95e181167b0fb6
Mikolaj <boronskimikolaj@gmail.com>
2023-04-10 23:16:56 +0200

    change sweep settings to better hyperparams, add notes to .gitignore

--- 01_andGate_wandb.py
+++ 01_andGate_wandb.py
@@ -52,36 +52,36 @@ def set_seed(seed: int = 42) -> None:
 
 sweep_config = {
     'method': 'grid',
-    'name': 'bRuTeFoRcE',
+    'name': 'bRuTeFoRcE_V2',
     'metric': {
         'goal': 'minimize',
         'name': 'train loss'
     },
-    "description": "Lineaer(2, 8), Linear(8, 2), Leaky()",
+    "description": "Lineaer(2, 8), Lineaer(8, 2) Leaky()",
     'parameters': {
         'learning_rate': {
-            'values': [0.0001, 0.01, 0.1]
+            'values': [0.01, 0.1]
         },
         'batch_size': {
             'values': [16, 32, 128]
         },
         'beta': {
-            'values': [0.1, 0.2, 0.5, 0.9]
+            'values': [0.5, 0.9]
         },
         'threshold': {
-            'values': [0.1, 0.2, 0.5, 0.9]
+            'values': [0.5, 0.9]
         },
         'timesteps': {
-            'values': [5, 10, 25]
+            'values': [5, 10]
         },
         'epochs': {
             'values': [30]
         },
         'rates': {
-            'values': [(0.9, 0.1), (0.7, 0.3)]
+            'values': [9, 7]
         },
         "surrogate": {
-            "values": ["fast_sigmoid", "sigmoid", "straight_through_estimator", "spike_rate_escape"] # "triangular" doesnt work for some reason
+            "values": ["fast_sigmoid", "sigmoid", "straight_through_estimator"] # "triangular" doesnt work for some reason, spike rate works bad
          },
         'seed': {
             'values': [1, 2137, 69]
@@ -95,6 +95,10 @@ surrogate_grads = {
     "straight_through_estimator": surrogate.straight_through_estimator(),
     "spike_rate_escape": surrogate.spike_rate_escape()
 }
+rates = {
+   9: (0.9, 0.1), 
+   7: (0.7, 0.3)
+}
 sweep_id = wandb.sweep(sweep_config, project='and-gate-snn')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -107,14 +111,14 @@ def train():
   test_loader = DataLoader(and_generator(size=300), config.batch_size)
 
   net = nn.Sequential(
-      nn.Linear(2, 8),
+      nn.Linear(2, 2),
       nn.Linear(8, 2),
       snn.Leaky(beta=config.beta, threshold=config.threshold, spike_grad=surrogate_grads[config.surrogate], init_hidden=True, output=True)
   )
   net = net.to(device)
 
   optimizer = torch.optim.Adam(net.parameters(), lr=config.learning_rate, betas=(0.9, 0.999))
-  loss_fn = SF.mse_count_loss(correct_rate=config.rates[0], incorrect_rate=config.rates[1])
+  loss_fn = SF.mse_count_loss(correct_rate=rates[config.rates][0], ratesincorrect_rate=rates[config.rates][1])
 
   for epoch in range(config.epochs):
     for i, (data, targets) in enumerate(iter(train_loader)):
