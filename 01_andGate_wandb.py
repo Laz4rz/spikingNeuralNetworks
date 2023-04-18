afde448c11c1792b7ca20356f581646d1e303b14
laz4rz <boronskimikolaj@gmail.com>
2023-04-18 16:50:01 +0200

    add other to .gitignore, modify 01_andGate_wandby.py to use sweep_config.py

--- 01_andGate_wandb.py
+++ 01_andGate_wandb.py
@@ -50,44 +50,7 @@ def set_seed(seed: int = 42) -> None:
     os.environ["PYTHONHASHSEED"] = str(seed)
     print(f"Random seed set as {seed}")
 
-sweep_config = {
-    'method': 'grid',
-    'name': 'bRuTeFoRcE_V2',
-    'metric': {
-        'goal': 'minimize',
-        'name': 'train loss'
-    },
-    "description": "Lineaer(2, 8), Lineaer(8, 2) Leaky()",
-    'parameters': {
-        'learning_rate': {
-            'values': [0.01, 0.1]
-        },
-        'batch_size': {
-            'values': [16, 32, 128]
-        },
-        'beta': {
-            'values': [0.5, 0.9]
-        },
-        'threshold': {
-            'values': [0.5, 0.9]
-        },
-        'timesteps': {
-            'values': [5, 10]
-        },
-        'epochs': {
-            'values': [30]
-        },
-        'rates': {
-            'values': [9, 7]
-        },
-        "surrogate": {
-            "values": ["fast_sigmoid", "sigmoid", "straight_through_estimator"] # "triangular" doesnt work for some reason, spike rate works bad
-         },
-        'seed': {
-            'values': [1, 2137, 69]
-        }
-    }
-}
+
 surrogate_grads = {
     "fast_sigmoid": surrogate.fast_sigmoid(),
     "triangular": surrogate.triangular(),
@@ -103,7 +66,7 @@ sweep_id = wandb.sweep(sweep_config, project='and-gate-snn')
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def train():
+def train_sweep():
   wandb.init()
   config = wandb.config
   set_seed(config.seed)
@@ -111,11 +74,9 @@ def train():
   test_loader = DataLoader(and_generator(size=300), config.batch_size)
 
   net = nn.Sequential(
-      nn.Linear(2, 8),
-      nn.Linear(8, 2),
+      nn.Linear(2, 2),
       snn.Leaky(beta=config.beta, threshold=config.threshold, spike_grad=surrogate_grads[config.surrogate], init_hidden=True, output=True)
-  )
-  net = net.to(device)
+  ).to(device)
 
   optimizer = torch.optim.Adam(net.parameters(), lr=config.learning_rate, betas=(0.9, 0.999))
   correct_rate, incorrect_rate = rates[config.rates]
@@ -139,4 +100,4 @@ def train():
     "accuracy": SF.accuracy_rate(spk_rec, targets)
     })
 
-wandb.agent(sweep_id, train)
+wandb.agent(sweep_id, train_sweep)
