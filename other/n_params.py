17d3c4c77c648166db17093b060f3409b53bf97f
Mikolaj <boronskimikolaj@gmail.com>
2023-06-14 20:05:41 +0200

    add n_params files

--- other/n_params.py
+++ other/n_params.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 
 import seaborn as sns
 
 sns.set_style("whitegrid")
 
-
+# set data
 data = [
     ("2018-06-11", "GPT-1", 0.12), 
     ("2018-10-31", "BERT-Large", 0.34), 
@@ -25,8 +26,45 @@ model = [d[1] for d in data]
 params = [d[2] for d in data]
 
 df = pd.DataFrame({"model": model, "params": params}, index=date)
+df["days"] = pd.to_datetime(df.reset_index()["index"]).diff().fillna(pd.Timedelta("0D")).dt.days.values.cumsum()
 
 
+# fit linear model
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+X = df["days"].values.reshape(-1, 1)
+y = df["params"].values.reshape(-1, 1) * 1e9
+
+model = make_pipeline(PolynomialFeatures(degree=1), LinearRegression())
+model.fit(X, y)
+
+print(model.named_steps["linearregression"].coef_)
+print(model.named_steps["linearregression"].intercept_)
+
+# get doubling time from model
+doubling_time = np.log(2) / model.named_steps["linearregression"].coef_[0][0]
+
+
+# from sklearn.linear_model import LinearRegression
+# from sklearn.preprocessing import PolynomialFeatures
+# from sklearn.pipeline import make_pipeline
+
+# X = df["days"].values.reshape(-1, 1)
+# y = df["params"].values.reshape(-1, 1)
+
+# model = make_pipeline(PolynomialFeatures(degree=1), LinearRegression())
+# model.fit(X, y)
+
+# print(model.named_steps["linearregression"].coef_)
+
+# # get doubling time from model
+# doubling_time = np.log(2) / model.named_steps["linearregression"].coef_[0][0]
+
+# print(f"Doubling timep: {doubling_time:.2f} days")
+
+# plot
 plt.figure(figsize=(10, 5))
 plt.title("Zmiana liczby parametrów sztucznych sieci neuronowych")
 plt.ylabel("Liczba parametrów (mld)")
@@ -39,7 +77,10 @@ for i in data:
     plt.annotate(i[1], xy=(i[0], i[2]), xytext=(i[0], i[2]+0.1*i[2]))
 
 
-plt.yscale("log")
+# plot log-linear model
+plt.plot(df.index, model.predict(X), linestyle="--", color="black")
+
+# plt.yscale("log")
 # plt.xlim(df.index[0], df.index[-1])
 plt.ylim(0.1, 1000)
 plt.axhline(y=brain[1], color="r", linestyle="--")
